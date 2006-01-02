23018fd63a47e5e07bbcc23695b8b033f1b15d25
enikao <enikao@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-01-02 13:54:04 +0000

    Added DataGridXML and tests

---
+++ trunk/core/XML/DataGridXML.class.php
@@ -0,0 +1,189 @@
+<?php
+/*
+ * Created on Dec 30, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+/**
+ * uses Serializer.php to structure Data in XML format.
+ */
+require_once 'Serializer.php';
+
+
+/**
+ * Supplies the data to a DataGrid in XML.
+ * 
+ * @author Eni Kao, Para Phil 
+ * @version $LastChangedRevision$
+ */
+class DataGridXML {
+
+	/**
+	 * Exception Code if the serializer returns an errror.  
+	 */
+	const XML_SERIALIZER_EXCEPTION = 4711;
+	
+	/**
+	 * Exception Code if getXML called with undefined columns.  
+	 */
+	const UNDEFINED_COLUMNS = 4712;
+	
+	/**
+	 * The XML Serializer Object.
+	 * 
+	 * @var object
+	 */
+	private $serializer;
+	
+	/**
+	 * All column heads in the table
+	 * 
+	 * @var simple string array of column names
+	 */
+	private $columns;
+	
+	/**
+	 * The single rows in the table.
+	 * 
+	 * @var array array of array of cell data
+	 */
+	private $rows;
+
+	/**
+	 * Initializes serializer and sets inital column and row data if given.
+	 * 
+	 * @param $columns array the columns to initialize the Object withh
+	 * @param $rows array the rows of the Object
+	 */
+	function __construct() {
+		$this->initSerializer();
+		$numArgs = func_num_args();
+		
+		// The constructor can't be overloaded the usual way so we have to use this workaround.		
+		if ($numArgs == 2) {
+			
+			// func_get_arg() can't be used as a function parameter.
+			$columns = func_get_arg(0);
+			$rows = func_get_arg(1);
+			$this->setData($columns, $rows);
+		}
+	}
+	
+	/**
+	 * Initialize the Serializer.
+	 * 
+	 * @return void
+	 */
+	private function initSerializer() {
+
+		// Makes the Serializer use sensible tag names.   
+		$options = array (
+			XML_SERIALIZER_OPTION_ROOT_NAME => "datatable",
+			XML_SERIALIZER_OPTION_DEFAULT_TAG => array (
+				'columns' => 'column',
+				'rows' => 'row',
+				'row' => 'cell'
+			)
+		);
+		
+    	// If the debug mode is on the xml will be formatted to a more pretty format.
+    	if (defined('BADGER_DEBUG')) {
+    		$options[XML_SERIALIZER_OPTION_INDENT] = '    ';
+			$options[XML_SERIALIZER_OPTION_LINEBREAKS] = "\n";
+    	}
+
+		$this->serializer = new XML_Serializer($options);
+	}
+	
+	/**
+	 * Sets the column head and row data.
+	 * 
+	 * @param $columns array array of strings of cloumn names
+	 * @param $rows array array of array of cell data
+	 * @return void 
+	 */
+	public function setData($columns, $rows) {
+		$this->setColumns($columns);
+		$this->setRows($rows);
+	}
+
+	/**
+	 * Sets the column head.
+	 * 
+	 * @param $columns array array of strings of cloumn names
+	 * @return void
+	 */
+	public function setColumns($columns) {
+		if (is_array($columns)) {
+			$this->columns = $columns;
+		}
+	}
+
+	/**
+	 * Sets the rows.
+	 *
+	 * @param $rows array array of array of cell data
+	 * @return void 
+	 */
+	public function setRows($rows) {
+		if (is_array($rows)) {
+			$this->rows = $rows;
+		}
+	}
+
+	/**
+	 * Adds a row.
+	 * 
+	 * Although works if no data has been given yet.
+	 * 
+	 * @param $rows array array of array of cell data
+	 * @return void 
+	 */
+	public function addRows($rows) {
+		
+		// checks if we have already data 
+		if (is_array($this->rows)) {
+			if (is_array($rows)) {
+				$this->rows = array_merge($this->rows, $rows);
+			}
+		} else {
+			$this->setRows($rows);
+		}
+	}
+	
+	/**
+	 * All rows will be erased.
+	 * 
+	 * @return void 
+	 */
+	public function emptyRows() {
+		$this->rows = null;
+	}		
+	
+	/**
+	 * Returns the XML structure.
+	 * 
+	 * @return string XML Structure
+	 */
+	public function getXML() {
+		if (!is_array($this->columns)) {
+			throw new Exception('getXML called with undefined columns', DataGridXML::UNDEFINED_COLUMNS);
+		}
+		
+		$data = array (
+			'columns' => $this->columns,
+			'rows' => $this->rows 
+		);
+	
+		$result = $this->serializer->serialize($data);
+		
+		if ($result === true) {
+			return $this->serializer->getSerializedData();
+		} else {
+			throw new Exception('Error while calling XML Serializer', DataGridXML::XML_SERIALIZER_EXCEPTION);
+		}
+	}
+}
+?>
---
+++ trunk/modules/moduleTests/DataGridXMLTest.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * Created on Dec 30, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+require_once '../../core/XML/DataGridXML.class.php';
+
+header('Content-Type: text/plain');
+
+function printDGX($dgx) {
+	$dgxResult = $dgx->getXML();
+	echo gettype($dgxResult);
+	echo "\n";
+	print_r($dgxResult);
+	echo "\n\n";
+}
+
+/* Constructor can't be tested alone because you need vaild columns for using
+ * getXML() or exception will be thrown!
+$dgx1 = new DataGridXML();
+echo "dgx1 constructor: \n";
+printDGX($dgx1);
+*/
+
+$columns1 = array ("Name", "Vorname", "Hund");
+$rows1 = array (
+	array ("Stets", "Niko", "Paul"),
+	array ("Hetscler", "Phil", "Ferdinand")
+);
+$columns2 = array ("Straße", "Hausnummer");
+$rows2 = array (
+	array ("Wilfried Ebert Str.", "14"),
+	array ("Hermann Str." , "25"),
+	array ("Stefan Kunklermannn Str.", "34")
+);
+
+$dgx2 = new DataGridXML($columns1, $rows1);
+echo "dgx2 constructor: \n";
+printDGX($dgx2);
+
+$dgx1 = new DataGridXML();
+echo "dgx1 setData: \n";
+$dgx1->setData($columns1, $rows1);
+printDGX($dgx1);
+
+echo "dgx1 setData: Overwrite \n";
+$dgx1->setData($columns2, $rows2);
+printDGX($dgx1);
+
+$dgx3 = new DataGridXML();
+echo "dgx3 setColumns: \n";
+$dgx3->setColumns($columns1);
+printDGX($dgx3);
+
+echo "dgx3 setColumns: Overwrite \n";
+$dgx3->setColumns($columns2);
+printDGX($dgx3);
+
+$dgx4 = new DataGridXML();
+echo "dgx4 setRows: \n";
+$dgx4->setColumns($columns1);
+$dgx4->setRows($rows1);
+printDGX($dgx4);
+
+echo "dgx4 setRows: Overwrite \n";
+$dgx4->setRows($rows2);
+printDGX($dgx4);
+
+$dgx5 = new DataGridXML();
+echo "dgx5 addRows to empty rows: \n";
+$dgx5->setColumns($columns1);
+$dgx5->addRows($rows1);
+printDGX($dgx5);
+
+echo "dgx5 addRows to filled rows: \n";
+$dgx5->addRows($rows1);
+printDGX($dgx5);
+
+echo "dgx5 emptyRows: \n";
+$dgx5->emptyRows();
+printDGX($dgx5);
+
+$dgx6 = new DataGridXML();
+echo "dgx6 getXML Exception Test \n";
+try {
+	$dgx6->getXML();
+	echo "no exception";
+} catch (Exception $ex) {
+	echo $ex;
+}	
+?>
