24bc26bdd4576344c3c29e20c905c7f49e5703bd
baxxter <baxxter@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-01-06 20:23:45 +0000

    html2pdf moved to includes from modules (because it ain't a module, ya know)

--- trunk/modules/html2pdf/stubs.common.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $Header: /cvsroot/html2ps/stubs.common.inc.php,v 1.1 2005/04/27 16:27:45 Konstantin Exp $
-
-if (!function_exists('file_get_contents')) {
-  require_once('stubs.file_get_contents.inc.php');
-}
-?>
