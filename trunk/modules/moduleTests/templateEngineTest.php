a0cd360f78a32517c480118269ee2234c1c9c3e1
sepp <sepp@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-01-07 18:55:59 +0000

    Test der TemplateEngine

---
+++ trunk/modules/moduleTests/templateEngineTest.php
@@ -0,0 +1,20 @@
+<?php
+/*
+* ____          _____   _____ ______ _____  
+*|  _ \   /\   |  __ \ / ____|  ____|  __ \ 
+*| |_) | /  \  | |  | | |  __| |__  | |__) |
+*|  _ < / /\ \ | |  | | | |_ |  __| |  _  / 
+*| |_) / ____ \| |__| | |__| | |____| | \ \ 
+*|____/_/    \_\_____/ \_____|______|_|  \_\
+* Open Source Financial Management
+* Visit http://badger.berlios.org 
+*
+**/
+define("BADGER_ROOT", "../../"); 
+require_once(BADGER_ROOT . "/includes/includes.php");
+  
+$tpl = new TemplateEngine("Standard", BADGER_ROOT);
+
+echo $tpl->getThemeName();
+echo $tpl->getTemplate("badgerFooter");
+?>
