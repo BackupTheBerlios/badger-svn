5cce0aa5bfdf656906144d05645ea09c005c8221
enikao <enikao@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-01-08 22:31:10 +0000

    Added navi SQL files

---
+++ techdoc/navi/table create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `navi` (
+`navi_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`parent_id` INT NOT NULL ,
+`menu_order` INT NOT NULL ,
+`item_type` CHAR( 1 ) NOT NULL ,
+`item_name` VARCHAR( 255 ) NULL ,
+`tooltip` VARCHAR( 255 ) NULL ,
+`icon_url` VARCHAR( 255 ) NULL ,
+`command` VARCHAR( 255 ) NULL 
+)
---
+++ techdoc/navi/table data.sql
@@ -0,0 +1,13 @@
+INSERT INTO `navi` VALUES (2, 0, 0, 'm', 'Datei', NULL, NULL, NULL);
+INSERT INTO `navi` VALUES (3, 2, 0, 'i', 'Neumachen macht viel Laune', NULL, NULL, 'http://www.spiegel.de/');
+INSERT INTO `navi` VALUES (4, 2, 1, 'i', 'Der Niko ist der Obermeister!!!!!', NULL, NULL, 'http://www.slashdot.org/');
+INSERT INTO `navi` VALUES (5, 2, 2, 'i', 'Speichern', NULL, NULL, 'http://www.heise.de/');
+INSERT INTO `navi` VALUES (6, 2, 3, 's', NULL, NULL, NULL, NULL);
+INSERT INTO `navi` VALUES (7, 2, 4, 'i', 'Beenden', NULL, NULL, 'javascript:alert(''Fort bisch!'');');
+INSERT INTO `navi` VALUES (8, 0, 1, 'm', 'Bearbeiten', NULL, NULL, NULL);
+INSERT INTO `navi` VALUES (9, 8, 0, 'm', 'Einfuegen als', NULL, NULL, NULL);
+INSERT INTO `navi` VALUES (10, 9, 0, 'i', 'Text', NULL, NULL, 'javascript:alert(''Text!'');');
+INSERT INTO `navi` VALUES (11, 9, 1, 'i', 'HTML', NULL, '', 'http://w3.org/');
+INSERT INTO `navi` VALUES (12, 0, 2, 'm', 'Konto', NULL, '{BADGER_ROOT}/modules/moduleTests/navi/account.png', NULL);
+INSERT INTO `navi` VALUES (13, 12, 0, 'i', 'Abbuchen', NULL, NULL, '{BADGER_ROOT}/modules/account/transfer.php');
+INSERT INTO `navi` VALUES (14, 0, 3, 'i', 'Ausloggen', NULL, '{BADGER_ROOT}/modules/moduleTests/navi/logout.png', 'javascript:alert(''Raus mit dir!'');');
