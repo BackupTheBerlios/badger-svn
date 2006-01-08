153e262e2e3f615218d2a6816de6e577ca617001
sepp <sepp@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-01-08 22:30:57 +0000

    /core/PEAR.php hinzugeügt

--- trunk/includes/includes.php
+++ trunk/includes/includes.php
@@ -12,10 +12,12 @@
 **/
  
  //Includes
- 
+
 /**
  * makes use of the dbAdapter database abstraction layer
  */
+ require_once BADGER_ROOT.'/core/PEAR.php';
+
  require_once BADGER_ROOT.'/core/dbAdapter/DB.php';
 
 /**
@@ -46,7 +48,7 @@ require_once BADGER_ROOT.'/core/Translation2/Translation2.php';
 /**
  * makes translation libs available
  */
-require_once BADGER_ROOT.'/includes/openDbConnection.php'; 
+//require_once BADGER_ROOT.'/includes/openDbConnection.php'; 
 
 /**
  * makes pdf generator libs available
