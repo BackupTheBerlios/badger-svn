432d0d3288c5e06c155754b4373552a0e9f620f9
sebastianeggers <sebastianeggers@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-01-09 22:20:57 +0000

    Added Login Module. Added Module Test for Login Module.

---
+++ trunk/includes/login.php
@@ -0,0 +1,43 @@
+<?php
+/*
+* ____          _____   _____ ______ _____  
+*|  _ \   /\   |  __ \ / ____|  ____|  __ \ 
+*| |_) | /  \  | |  | | |  __| |__  | |__) |
+*|  _ < / /\ \ | |  | | | |_ |  __| |  _  / 
+*| |_) / ____ \| |__| | |__| | |____| | \ \ 
+*|____/_/    \_\_____/ \_____|______|_|  \_\
+* Open Source Finance Management
+* Visit http://badger.berlios.org 
+*
+**/
+
+//Include Session Management
+include(BADGER_ROOT . "/core/SessionManager/session.ses.php");
+
+//placeholder for pw from db
+$readoutpassword = md5("hoerner");
+$passwordcorrect = false;
+if (isset($_session['password']) && $readoutpassword == $_session['password'])
+	{$passwordcorrect = true;
+	}
+	elseif(isset($_POST['password']) && md5($_POST['password']) == $readoutpassword )
+		{
+			$passwordcorrect = true;
+			//create session variable
+			set_session_var('password',md5($_POST['password']));
+		};
+
+if($passwordcorrect == false)
+	{
+		print("<form method=\"post\" action=\"".$PHP_SELF."\">");
+		print("<input name=\"password\" id=\"password\" size=\"50\" maxlength=\"150\" value=\"\" type=\"password\"><br>");
+		print("<input value=\"Go!\" name=\"submit\" type=\"submit\">");
+		print("</form>");
+		exit();
+	};
+
+
+
+
+require_once(BADGER_ROOT . "/includes/fileFooter.php");
+?>
---
+++ trunk/modules/moduleTests/LoginTest.php
@@ -0,0 +1,22 @@
+<?php
+/*
+* ____          _____   _____ ______ _____  
+*|  _ \   /\   |  __ \ / ____|  ____|  __ \ 
+*| |_) | /  \  | |  | | |  __| |__  | |__) |
+*|  _ < / /\ \ | |  | | | |_ |  __| |  _  / 
+*| |_) / ____ \| |__| | |__| | |____| | \ \ 
+*|____/_/    \_\_____/ \_____|______|_|  \_\
+* Open Source Finance Management
+* Visit http://badger.berlios.org 
+*
+**/
+define("BADGER_ROOT", "../.."); 
+require_once(BADGER_ROOT . "/includes/includes.php");
+
+require(BADGER_ROOT . "/includes/login.php");
+
+print("If you can see this text, you are logged.<br>");
+print("<a href=\"LoginTest.php\">link</a><br>");
+
+require_once(BADGER_ROOT . "/includes/fileFooter.php");
+?>
