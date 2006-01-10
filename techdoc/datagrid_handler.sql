428aa132da2747c6afe8d081cc73d4b5548b75a7
enikao <enikao@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-01-10 23:38:22 +0000

    Added datagrid_handler.sql

---
+++ techdoc/datagrid_handler.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `datagrid_handler` (
+`handler_name` VARCHAR( 100 ) NOT NULL ,
+`file_path` VARCHAR( 255 ) NOT NULL ,
+`class_name` VARCHAR( 100 ) NOT NULL ,
+PRIMARY KEY ( `handler_name` )
+);
