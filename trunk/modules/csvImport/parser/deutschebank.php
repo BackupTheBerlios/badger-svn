99077110d580eb1f3752ee7828b8f32ec47339f9
sperber <sperber@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-01-29 18:13:59 +0000

    csv import

--- trunk/modules/csvImport/parser/deutschebank.php
+++ trunk/modules/csvImport/parser/deutschebank.php
@@ -23,22 +23,53 @@ function parseToArray($fp, $accountId){
 		 * @var int 
 		 */
 		$csvRow = 0;
+		/**
+		 * is set true, a line contains ; , but not the correct number for this parser (5)
+		 * 
+		 * @var boolean 
+		 */
+		$noValidFile = NULL;
+		/**
+		 * is set true, after the header was ignored
+		 * 
+		 * @var boolean 
+		 */
+		$headerIgnored = NULL;
 		//for every line
 		while (!feof($fp)) {
 			//read one line
+			$rowArray = NULL;
+			//ignore header (first 5 lines)
+			if (!$headerIgnored){
+				for ($headerLine = 0; $headerLine < 5; $headerLine++) {
+					$garbage = fgets($fp, 1024);
+					//to ignore this code on the next loop run
+					$headerIgnored = true;
+				}
+			}
+			//read one line
 			$line = fgets($fp, 1024);
-			if (strstr($line, ";")) { //if line is not empty
+			//if line is not empty or is no header
+			if (strstr($line, ";")) { 
+				//if line contains excactly 5 ';', to ensure it is a valid Deutsche Bank csv file
+				if (substr_count ($line, ";")==5){ 
+					// divide String to an array
 					$transactionArray = explode(";", $line);
+					//format date
 					$valutaDate = explode(".", $transactionArray[0]); //Valuta Date
 					$valutaDate[2] = $valutaDate[2];
 					$valutaDate[4] = $valutaDate[2] . "-" . $valutaDate[1] . "-" . $valutaDate[0];
+					//to avoid '/' in the title
 					$transactionArray[2] = str_replace("\"","",$transactionArray[2]); //title
 					//format amount data
 					$transactionArray[3] = str_replace(",",".",$transactionArray[3]); 
 					$transactionArray[4] = str_replace(",",".",$transactionArray[4]);
-					//if transactionArray[3] == "", it is an expenditure
-					if ($transactionArray[3]=="") { $amount = $transactionArray[4];}
-					else {$amount = "-" . $transactionArray[3];}				
+					//if transactionArray[3] == "", it is an expenditure, else income
+					if ($transactionArray[3]=="") { 
+						$amount = $transactionArray[4];
+					} else {
+						$amount = $transactionArray[3];
+					}				
 					/**
 					 * transaction array
 					 * 
@@ -53,19 +84,38 @@ function parseToArray($fp, $accountId){
 					   "amount" => $amount,
 					   "transactionPartner" => ""
 					);
-
+				} else{
+					$noValidFile = 'true';
+				}
 			}
-			/**
-			 * array of all transaction arrays
-			 * 
-			 * @var array
-			 */
-			$importedTransactions[$csvRow] = $rowArray;
-
-			$csvRow++;
-			echo $csvRow;
+			// if a row contains valid data
+			if ($rowArray){
+				/**
+				 * array of all transaction arrays
+				 * 
+				 * @var array
+				 */
+				$importedTransactions[$csvRow] = $rowArray;
+				$csvRow++;
+			}
 
 		}
-		return $importedTransactions;
+		if ($noValidFile) {
+			echo "du bist echt super doof";
+			//close file
+			fclose ($fp);
+		} else {
+			if ($csvRow == 0){
+				echo "du bist doof";
+				//close file
+				fclose ($fp);
+			} else{
+				//delete footer (1 line)
+				unset($importedTransactions[$csvRow-1]);
+				//close file
+				fclose ($fp);
+				return $importedTransactions;
+			}
+		}
 }
 ?>
