d276909d811678dce8c7b9b15cd84df656cf63d1
sepp <sepp@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-02-04 11:09:04 +0000

    new onload events

--- trunk/core/widgets/DataGrid.class.php
+++ trunk/core/widgets/DataGrid.class.php
@@ -19,13 +19,17 @@ class DataGrid {
 private $tpl;
 public $headerName = array();
 public $headerSize = array();
-
+public $deleteMsg;
+public $deleteAction;
+public $editAction;
 
 	public function __construct($tpl) {
 		$this->tpl = $tpl;
 	}
 	
 	public function writeHeader() {
+		//toDo: Use templateEngine
+		
 		$output = '<div id="dataGrid">
 					<table id="dgTableHead" cellpadding="2" cellspacing="0">
 						<tr>
@@ -36,6 +40,20 @@ public $headerSize = array();
 		$output .= '		<td></td>
 						</tr>
 					</table>';
+					
+		$output .= '<div id="dgScroll">
+					<table id="dgData" cellpadding="2" cellspacing="0" rules="row">
+						<tbody>
+					
+						</tbody>
+					</table>
+					</div>
+					<table id="dgTableFoot" cellpadding="2" cellspacing="0">
+						<tr>
+							<td>xx Datensätze</td>
+						</tr>
+					</table>
+					</div>';
 		return $output;		
 	}
 	
@@ -43,6 +61,13 @@ public $headerSize = array();
 		$this->tpl->addJavaScript("js/dataGrid.js");
 		$this->tpl->addOnLoadEvent('dgHeaderName = new Array("'.implode('","',$this->headerName).'");');
 		$this->tpl->addOnLoadEvent('dgHeaderSize = new Array('.implode(',',$this->headerSize).');');
+		$this->tpl->addOnLoadEvent('dgDeleteMsg = "'. $this->deleteMsg .'";');
+		$this->tpl->addOnLoadEvent('dgDeleteAction = "'. $this->deleteAction .'";');
+		$this->tpl->addOnLoadEvent('dgEditAction = "'. $this->editAction .'";');
+		$this->tpl->addOnLoadEvent('dgInit();');
+		$this->tpl->addOnLoadEvent('Behaviour.register(behaviour);');
+		$this->tpl->addOnLoadEvent('Behaviour.apply();');
+		
 	}
 }
 ?>
