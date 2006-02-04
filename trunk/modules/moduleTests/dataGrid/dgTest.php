c5043d1ecad39cb11fcd051eeb07255a0d15f748
sepp <sepp@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-02-04 11:09:26 +0000

    working account-manager example

--- trunk/modules/moduleTests/dataGrid/dgTest.php
+++ trunk/modules/moduleTests/dataGrid/dgTest.php
@@ -19,70 +19,62 @@ $tpl->addJavaScript("js/behaviour.js");
 $tpl->addJavaScript("js/prototype.js");
 
 $dataGrid = new DataGrid($tpl);
-$dataGrid->headerName = array("accountId","currency","title","balance"); 
-$dataGrid->headerSize = array(100,150,200,180);
 $dataGrid->sourceXML = BADGER_ROOT."/core/XML/getDataGridXML.php?q=AccountManager";
+$dataGrid->headerName = array("currency","title","balance"); 
+$dataGrid->headerSize = array(150,200,180);
+$dataGrid->deleteMsg = "Wollen sie die Datensätze wirklich löschen?"; //TODO Translation
+$dataGrid->deleteAction = "xyp.php";
+$dataGrid->editAction = "xyp.php?id=";
 $dataGrid->initDataGridJS();
 echo $tpl->getHeader("DataGrid");
 
 echo $dataGrid->writeHeader();
 ?>
-<div id="dgScroll">
-<table id="dgData" cellpadding="2" cellspacing="0" rules="row">
-	<tbody>
-		<tr class="dgRow" id="0">
-			<td width="20"><input type="checkbox" name="1" value="ON" id="check1"/></td>
-			<td width="100">Name1</td>
-			<td width="150">Vorname</td>
-			<td width="200">Alter</td>
-			<td width="180">Kontostand</td>
-			<td>&nbsp;</td>
-		</tr>
-	</tbody>
-</table>
-</div>
-<table id="dgTableFoot" cellpadding="2" cellspacing="0">
-	<tr>
-		<td>xx Datensätze</td>
-	</tr>
-</table>
-</div>
+
 <script>
-window.setTimeout('dgInit()', 1000);
+
 
 function dgInit() {
-	//xmlDoc = loadData('<?php echo $dataGrid->sourceXML?>');
-	//alert(xmlDoc);
+	xmlDoc = loadData('<?php echo $dataGrid->sourceXML?>');
+	xmlRows = xmlDoc.getElementsByTagName("row");
 	
-	rowID = 10;
-	dgData = document.getElementById("dgData");
-	newRow = document.createElement("tr");
-	newRow.className = "dgRow";
-	newRow.id=rowID;
+	dgData = document.getElementById("dgData"); //.getElementsByTagName("tbody")[0];
 	
-	//checkbox
-	checkTD = document.createElement("td");
-	checkTD.width="20";
-	checkBox = document.createElement("input");
-	checkBox.id = "check" + rowID;
-	checkBox.name = "check" + rowID;
-	checkBox.type = "checkbox";
-	checkTD.appendChild(checkBox);
-	newRow.appendChild(checkTD);
-	
-	//values
-	for (i=0; i<dgHeaderSize.length; i++) {
-		cell = document.createElement("td");
-		cell.width = dgHeaderSize[i];
+	for (j=0; j<xmlRows.length; j++) {
+		cells = xmlRows[j].getElementsByTagName("cell");
+		rowID = cells[0].textContent;
 		
-		cell.innerHTML = dgHeaderSize[i];
+		newRow = document.createElement("tr");
+		newRow.className = "dgRow";
+		newRow.id=rowID;
 		
-		newRow.appendChild(cell);
-	} 
-	
-	dgData.appendChild(newRow);
-
+		//checkbox
+		checkTD = document.createElement("td");
+		checkTD.width="20";
+		checkBox = document.createElement("input");
+		checkBox.id = "check" + rowID;
+		checkBox.name = "check" + rowID;
+		checkBox.type = "checkbox";
+		checkTD.appendChild(checkBox);
+		newRow.appendChild(checkTD);
+		
+		//values
+		for (i=0; i<dgHeaderSize.length; i++) {
+			cell = document.createElement("td");
+			cell.width = dgHeaderSize[i];			
+			cell.innerHTML = cells[i+1].textContent;			
+			newRow.appendChild(cell);
+		} 
+		
+		//empty cell
+		lastTD = document.createElement("td");
+		lastTD.innerHTML = "&nbsp;";
+		newRow.appendChild(lastTD);
+		
+		dgData.appendChild(newRow);
+	}
 }
+
 </script>
 
 
