fbcd8be63ebdf309a3d4936e4ee25f57c1b55d35
baxxter <baxxter@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-02-06 21:26:25 +0000

    added more graph examples

---
+++ trunk/modules/moduleTests/chartTest.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* ____          _____   _____ ______ _____  
+*|  _ \   /\   |  __ \ / ____|  ____|  __ \ 
+*| |_) | /  \  | |  | | |  __| |__  | |__) |
+*|  _ < / /\ \ | |  | | | |_ |  __| |  _  / 
+*| |_) / ____ \| |__| | |__| | |____| | \ \ 
+*|____/_/    \_\_____/ \_____|______|_|  \_\
+* Open Source Financial Management
+* Visit http://badger.berlios.org 
+*
+**/
+define("BADGER_ROOT", "../..");
+require_once(BADGER_ROOT . "/includes/fileHeaderFrontEnd.inc.php");
+
+$widgets = new WidgetEngine($tpl); 
+$widgets->addToolTipJS();
+$widgets->addCalendarJS();
+
+$widgets->addNavigationHead();
+echo $tpl->getHeader("Charts");
+echo $widgets->getNavigationBody();
+
+//include charts.php to access the InsertChart function
+require_once(BADGER_ROOT . "/includes/charts/charts.php");
+
+echo InsertChart ( BADGER_ROOT . "/includes/charts/charts.swf", BADGER_ROOT . "/includes/charts/charts_library", BADGER_ROOT . "/modules/moduleTests/chartTestBar.php" );
+echo "<br /><br />";
+echo InsertChart ( BADGER_ROOT . "/includes/charts/charts.swf", BADGER_ROOT . "/includes/charts/charts_library", BADGER_ROOT . "/modules/moduleTests/chartTestLine.php" );
+echo "<br /><br />";
+echo InsertChart ( BADGER_ROOT . "/includes/charts/charts.swf", BADGER_ROOT . "/includes/charts/charts_library", BADGER_ROOT . "/modules/moduleTests/chartTestPie.php" );
+echo "<br /><br />";
+
+eval("echo \"".$tpl->getTemplate("badgerFooter")."\";");
+?>
---
+++ trunk/modules/moduleTests/chartTestPie.php
@@ -0,0 +1,11 @@
+<?php
+
+//include charts.php to access the SendChartData function
+require_once(BADGER_ROOT . "/includes/charts/charts.php");
+
+//change the chart to a bar chart
+$chart [ 'chart_type' ] = "pie";
+
+SendChartData ();
+
+?>
