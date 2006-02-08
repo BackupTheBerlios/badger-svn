0cfee35a2f4e8a50ebd119d8cd1914f8ea76dfd0
sepp <sepp@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-02-08 14:56:41 +0000

    column width

--- trunk/modules/account/AccountManagerOverview.php
+++ trunk/modules/account/AccountManagerOverview.php
@@ -24,7 +24,7 @@ $dataGrid->sourceXML = BADGER_ROOT."/core/XML/getDataGridXML.php?q=AccountManage
 $dataGrid->initialSort = "&ok0=title&od0=a";
 $dataGrid->headerName = array("Title","Balance","Currency");
 $dataGrid->columnOrder = array("title","balance","currency");  
-$dataGrid->headerSize = array(200,150,30);
+$dataGrid->headerSize = array(200,150,100);
 $dataGrid->cellAlign = array("left","right","left");
 $dataGrid->deleteMsg = getBadgerTranslation2('dataGrid', 'deleteMsg');
 $dataGrid->rowCounterName = getBadgerTranslation2('dataGrid', 'rowCounterName');
