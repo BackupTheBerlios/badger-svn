675cd5fc9fcc4f5ace2821bb556999cd7c53971c
sepp <sepp@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-02-10 14:09:12 +0000

    new structure

--- trunk/modules/account/CurrencyManager.php
+++ trunk/modules/account/CurrencyManager.php
@@ -14,9 +14,8 @@ define("BADGER_ROOT", "../..");
 require_once(BADGER_ROOT . "/includes/fileHeaderFrontEnd.inc.php");
 require_once(BADGER_ROOT . '/modules/account/CurrencyManager.class.php');
 
-$cm = new CurrencyManager($badgerDb);
-$widgets = new WidgetEngine($tpl);
-$widgets->addToolTipJS();
+$redirectPageAfterSave = "CurrencyManagerOverview.php";
+$pageTitle = "Currency Manager"; //I18N
 
 if (isset($_GET['action'])) {
 	switch ($_GET['action']) {
@@ -24,59 +23,81 @@ if (isset($_GET['action'])) {
 			//background delete
 			//called by dataGrid
 			if (isset($_GET['ID'])) {
-				$ID = $_GET['ID'];
+				$ID = $_GET['ID'];				
+				//check if we can delete this item (or is the currency used)
 				$cm->deleteCurrency($ID);
 				//catch error?
 			} else {
 				//error: no ID	
 			}			
+			break;
+		case 'save':
+			//add record, update record
+			updateRecord();
 			break;		
 		case 'new':
 		case 'edit':
-			//background activity   add record, update record
-			if (isset($_POST['hiddenID'])) {
-				switch ($_POST['hiddenID']) {
-				case 'new':
-					//add new record
-					//check if $_POST['symbol'], $_POST['longName'] is set?????
-					$ID = $cm->addCurrency($_POST['symbol'], $_POST['longname']);
-					break;
-				default:
-					//update record
-					$currency = $cm->getCurrencyById($_POST['hiddenID']);
-					$currency->setSymbol($_POST['symbol']);
-					$currency->setLongName($_POST['longname']);
-					$ID = $currency->getId();
-				}				
-			}
-			
-			//frontend form
-			echo $tpl->getHeader("CurrencyManager - New");
-			echo $widgets->addToolTipLayer();
-			if (isset($_GET['ID']) || $ID) {
-				//edit: load values for this ID
-				if (!$ID) {$ID = $_GET['ID'];};
-				$currency = $cm->getCurrencyById($ID);
-				$symbolValue = $currency->getSymbol();
-				$langnameValue = $currency->getLongName();				
-			} else {
-				//new: empty values
-				$ID = "new";
-				$symbolValue = "";
-				$langnameValue = ""; 
-			}
-			//set vars with values
-			$FormAction = $_SERVER['PHP_SELF'];
-			$hiddenID = $widgets->createField("hiddenID", 20, $ID, "", false, "hidden");
-			$symbolLabel = $widgets->createLabel("symbol", getBadgerTranslation2('accountCurrency', 'symbol'), true);
-			$symbolField = $widgets->createField("symbol", 20, $symbolValue, "ToolTip I18N", true, "text", "");
-			$longnameLabel = $widgets->createLabel("longname", getBadgerTranslation2('accountCurrency', 'longname'), true);
-			$longnameField = $widgets->createField("longname", 20, $langnameValue, "ToolTip I18N", true, "text", "");
-			$submitBtn = $widgets->createButton("submit", "Speichern I18N", "submit", "Widgets/accept.gif");
-
-			//add vars to template, print site
-			eval("echo \"".$tpl->getTemplate("Account/Currency")."\";");
-
+			//frontend form for edit or insert
+			printFrontend();
 			break;
+	}	
+}
+function printFrontend() {
+	global $pageTitle;
+	global $tpl;
+	global $badgerDb;
+	$widgets = new WidgetEngine($tpl);
+	$widgets->addToolTipJS();
+	$cm = new CurrencyManager($badgerDb);
+	
+	echo $tpl->getHeader($pageTitle);
+	echo $widgets->addToolTipLayer();
+	if (isset($_GET['ID'])) {
+		//edit: load values for this ID
+		$ID = $_GET['ID'];
+		$currency = $cm->getCurrencyById($ID);
+		$symbolValue = $currency->getSymbol();
+		$langnameValue = $currency->getLongName();				
+	} else {
+		//new: empty values
+		$ID = "new";
+		$symbolValue = "";
+		$langnameValue = ""; 
+	}
+	//set vars with values
+	$FormAction = $_SERVER['PHP_SELF'];
+	$hiddenID = $widgets->createField("hiddenID", 20, $ID, "", false, "hidden");
+	$symbolLabel = $widgets->createLabel("symbol", getBadgerTranslation2('accountCurrency', 'symbol'), true);
+	$symbolField = $widgets->createField("symbol", 20, $symbolValue, "", true, "text", "maxlength='3'");
+	$longnameLabel = $widgets->createLabel("longname", getBadgerTranslation2('accountCurrency', 'longname'), true);
+	$longnameField = $widgets->createField("longname", 20, $langnameValue, "", true, "text", "");
+	$submitBtn = $widgets->createButton("submit", getBadgerTranslation2('dataGrid', 'save'), "submit", "Widgets/accept.gif");
+
+	//add vars to template, print site
+	eval("echo \"".$tpl->getTemplate("Account/Currency")."\";");
+}
+
+
+function updateRecord() {
+	global $redirectPageAfterSave;
+	global $badgerDb;
+	$cm = new CurrencyManager($badgerDb);
+	
+	if (isset($_POST['hiddenID'])) {
+		switch ($_POST['hiddenID']) {
+		case 'new':
+			//add new record
+			//check if $_POST['symbol'], $_POST['longName'] is set?????
+			$ID = $cm->addCurrency($_POST['symbol'], $_POST['longname']);
+			break;
+		default:
+			//update record
+			$currency = $cm->getCurrencyById($_POST['hiddenID']);
+			$currency->setSymbol($_POST['symbol']);
+			$currency->setLongName($_POST['longname']);
+			$ID = $currency->getId();
+		}
+		//REDIRECT
+		header("Location: $redirectPageAfterSave");
 	}	
 }
