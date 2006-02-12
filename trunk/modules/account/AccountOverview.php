a7b1552f9bbee0f569a084f884d152ca42be7146
sepp <sepp@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-02-12 09:42:00 +0000

    column width

--- trunk/modules/account/AccountOverview.php
+++ trunk/modules/account/AccountOverview.php
@@ -20,12 +20,18 @@ $tpl->addJavaScript("js/behaviour.js");
 $tpl->addJavaScript("js/prototype.js");
 
 $dataGrid = new DataGrid($tpl);
-$dataGrid->sourceXML = BADGER_ROOT."/core/XML/getDataGridXML.php?q=Account&qp=1";
+if (isset($_GET['accountID'])) {
+	$accountID=$_GET['accountID'];
+} else {
+	throw new badgerException('accountOverview', 'noAccountID', '');
+}
+
+$dataGrid->sourceXML = BADGER_ROOT."/core/XML/getDataGridXML.php?q=Account&qp=$accountID";
 $dataGrid->headerName = array("Title","type","description","valutaDate","amount","categoryTitle");
 $dataGrid->columnOrder = array("title","type","description","valutaDate","amount","categoryTitle");  
 $dataGrid->initialSort = "title";
-$dataGrid->headerSize = array(200,200,180,150,150,150);
-$dataGrid->cellAlign = array("left","left","left","right","right","left");
+$dataGrid->headerSize = array(200,200,180,85,150,200);
+$dataGrid->cellAlign = array("left","left","left","left","right","left");
 $dataGrid->deleteMsg = getBadgerTranslation2('dataGrid', 'deleteMsg');
 $dataGrid->rowCounterName = getBadgerTranslation2('dataGrid', 'rowCounterName');
 $dataGrid->deleteAction = "deleteXYZ.php";
