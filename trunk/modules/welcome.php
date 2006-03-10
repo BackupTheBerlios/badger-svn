d90b4edf3f97db387fc1b8f5b0ddfeee9b55d835
togro82 <togro82@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-03-10 12:13:27 +0000

    I18n

--- trunk/modules/welcome.php
+++ trunk/modules/welcome.php
@@ -17,14 +17,21 @@ $tpl->addCSS("style.css");
 $widgets = new WidgetEngine($tpl); 
 $widgets->addToolTipJS();
 
+$Kontenübersicht = getBadgerTranslation2 ('accountAccount', 'pageTitleOverview');
+$Kategorienübersicht = getBadgerTranslation2 ('accountCategory', 'pageTitleOverview');
+$Statistiken = getBadgerTranslation2 ('statistics','pageTitle');
+$Backup = getBadgerTranslation2 ('importExport', 'askTitle');
+$Benutzereinstellungen = getBadgerTranslation2 ('UserSettingsAdmin', 'title');
+
+
 $widgets->addNavigationHead();
 echo $tpl->getHeader("Badger");
 
-$Modul1 = "<a href='".BADGER_ROOT."/modules/account/AccountManagerOverview.php'>Kontenübersicht</a>";
-$Modul2 = "<a href='".BADGER_ROOT."/modules/account/CategoryManagerOverview.php'>Kategorienübersicht</a>";
-$Modul3 = "<a href='".BADGER_ROOT."/modules/statistics/statistics.php'>Statistiken</a>";
-$Modul4 = "<a href='".BADGER_ROOT."/modules/importExport/importExport.php?mode=export'>Backup</a>";
-$Modul5 = "<a href='".BADGER_ROOT."/core/UserSettingsAdmin/UserSettingsAdmin.php'>Benutzereinstellungen</a>";
+$Modul1 = "<a href='".BADGER_ROOT."/modules/account/AccountManagerOverview.php'>$Kontenübersicht</a>";
+$Modul2 = "<a href='".BADGER_ROOT."/modules/account/CategoryManagerOverview.php'>$Kategorienübersicht</a>";
+$Modul3 = "<a href='".BADGER_ROOT."/modules/statistics/statistics.php'>$Statistiken</a>";
+$Modul4 = "<a href='".BADGER_ROOT."/modules/importExport/importExport.php?mode=export'>$Backup</a>";
+$Modul5 = "<a href='".BADGER_ROOT."/core/UserSettingsAdmin/UserSettingsAdmin.php'>$Benutzereinstellungen</a>";
 
 eval("echo \"".$tpl->getTemplate("badgerOverview")."\";");
 
