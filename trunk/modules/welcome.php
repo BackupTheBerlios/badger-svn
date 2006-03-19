510df9edad6a3cfaa74530d0710252ca8673a066
sepp <sepp@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-03-19 16:00:28 +0000

    name of vars -> english

--- trunk/modules/welcome.php
+++ trunk/modules/welcome.php
@@ -17,21 +17,21 @@ $tpl->addCSS("style.css");
 $widgets = new WidgetEngine($tpl); 
 $widgets->addToolTipJS();
 
-$Kontenübersicht = getBadgerTranslation2 ('accountAccount', 'pageTitleOverview');
-$Kategorienübersicht = getBadgerTranslation2 ('accountCategory', 'pageTitleOverview');
-$Statistiken = getBadgerTranslation2 ('statistics','pageTitle');
-$Backup = getBadgerTranslation2 ('importExport', 'askTitle');
-$Benutzereinstellungen = getBadgerTranslation2 ('UserSettingsAdmin', 'title');
+$accountOverview = getBadgerTranslation2 ('accountAccount', 'pageTitleOverview');
+$categoryOverview = getBadgerTranslation2 ('accountCategory', 'pageTitleOverview');
+$statistics = getBadgerTranslation2 ('statistics','pageTitle');
+$backup = getBadgerTranslation2 ('importExport', 'askTitle');
+$userPrefrences = getBadgerTranslation2 ('UserSettingsAdmin', 'title');
 
 
 $widgets->addNavigationHead();
 echo $tpl->getHeader("Badger");
 
-$Modul1 = "<a href='".BADGER_ROOT."/modules/account/AccountManagerOverview.php'>$Kontenübersicht</a>";
-$Modul2 = "<a href='".BADGER_ROOT."/modules/account/CategoryManagerOverview.php'>$Kategorienübersicht</a>";
-$Modul3 = "<a href='".BADGER_ROOT."/modules/statistics/statistics.php'>$Statistiken</a>";
-$Modul4 = "<a href='".BADGER_ROOT."/modules/importExport/importExport.php?mode=export'>$Backup</a>";
-$Modul5 = "<a href='".BADGER_ROOT."/core/UserSettingsAdmin/UserSettingsAdmin.php'>$Benutzereinstellungen</a>";
+$Modul1 = "<a href='".BADGER_ROOT."/modules/account/AccountManagerOverview.php'>$accountOverview</a>";
+$Modul2 = "<a href='".BADGER_ROOT."/modules/account/CategoryManagerOverview.php'>$categoryOverview</a>";
+$Modul3 = "<a href='".BADGER_ROOT."/modules/statistics/statistics.php'>$statistics</a>";
+$Modul4 = "<a href='".BADGER_ROOT."/modules/importExport/importExport.php?mode=export'>$backup</a>";
+$Modul5 = "<a href='".BADGER_ROOT."/core/UserSettingsAdmin/UserSettingsAdmin.php'>$userPrefrences</a>";
 
 eval("echo \"".$tpl->getTemplate("badgerOverview")."\";");
 
