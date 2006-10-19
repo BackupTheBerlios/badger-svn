c07c9dd76cdabb65a2634bb6331243f473248ebd
sepp <sepp@2019d85c-0f08-0410-861b-a097f8bc85d7>
2006-10-19 19:11:57 +0000

    fixed buttons events

--- trunk/modules/account/CurrencyManagerOverview.php
+++ trunk/modules/account/CurrencyManagerOverview.php
@@ -38,11 +38,11 @@ echo $tpl->getHeader($pageTitle);
 
 echo "<h1>$pageTitle</h1>";
 
-echo $widgets->createButton("btnNew", getBadgerTranslation2('dataGrid', 'new'), "CurrencyManager.callNewEvent()", "Widgets/table_add.gif");
+echo $widgets->createButton("btnNew", getBadgerTranslation2('dataGrid', 'new'), "dataGridCurrencyManager.callNewEvent()", "Widgets/table_add.gif");
 echo ' ';
-echo $widgets->createButton("btnEdit", getBadgerTranslation2('dataGrid', 'edit'), "CurrencyManager.callEditEvent()", "Widgets/table_edit.gif");
+echo $widgets->createButton("btnEdit", getBadgerTranslation2('dataGrid', 'edit'), "dataGridCurrencyManager.callEditEvent()", "Widgets/table_edit.gif");
 echo ' ';
-echo $widgets->createButton("btnDelete", getBadgerTranslation2('dataGrid', 'delete'), "CurrencyManager.callDeleteEvent()", "Widgets/table_delete.gif");
+echo $widgets->createButton("btnDelete", getBadgerTranslation2('dataGrid', 'delete'), "dataGridCurrencyManager.callDeleteEvent()", "Widgets/table_delete.gif");
 		
 echo $dataGrid->writeDataGrid();
 
