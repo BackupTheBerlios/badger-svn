aefe7e6b63cb8b377255513a8669ad6de971a48c
enikao <enikao@2019d85c-0f08-0410-861b-a097f8bc85d7>
2007-01-21 20:59:12 +0000

    Added .htaccess with protection of log files; renamed log file

--- trunk/includes/config.inc.php
+++ trunk/includes/config.inc.php
@@ -57,7 +57,7 @@
 	 * 
 	 * @var global constant
 	 */	
- define("LOG_FILE_NAME", BADGER_ROOT . "/.htlog");
+ define("LOG_FILE_NAME", BADGER_ROOT . "/badger.log");
  
   	/**
 	 * Formatting for date used in log entries according to http://www.php.net/date
